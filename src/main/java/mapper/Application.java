06d2550a75858314da4929cb9e5973f77b98339f
weiyun <940915753@qq.com>
2019-12-13 17:26:42 +0800

    Initial commit

---
+++ src/main/java/mapper/Application.java
@@ -0,0 +1,19 @@
+package mapper;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@MapperScan("mapper")
+@SpringBootApplication
+public class Application{
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
+
+
---
+++ src/main/java/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import models.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+
+
+
+}
